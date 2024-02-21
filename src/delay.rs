d447eb01220298f8940757857782cc977e3df652
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2024-02-21 09:38:05 -0800

    Add delay using window

--- src/delay.rs
+++ src/delay.rs
@@ -27,3 +27,24 @@ impl<D: Into<std::time::Duration>> DelayStrategy<D> for TokioSleep {
         tokio::time::sleep(delay.into())
     }
 }
+
+#[cfg(feature = "async-web")]
+impl<D: Into<std::time::Duration>> DelayStrategy<D> for web_sys::Window {
+    type Out = Box<dyn std::future::Future<Output = ()> + Unpin>;
+
+    fn delay(&self, by: D) -> Self::Out {
+        use wasm_bindgen_futures::{js_sys::Promise, JsFuture};
+
+        let window = self.clone();
+        let delay = by.into().as_millis() as i32;
+        Box::new(Box::pin(async move {
+            JsFuture::from(Promise::new(&mut |resolve, _reject| {
+                window
+                    .set_timeout_with_callback_and_timeout_and_arguments_0(&resolve, delay)
+                    .unwrap();
+            }))
+            .await
+            .unwrap();
+        }))
+    }
+}
